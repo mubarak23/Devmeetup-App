d79105c0dc71906cfe89638737eb421df29f25f2
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-09-26 16:42:58 +0100

    setup the navbar with side drawer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <v-app>
-    <v-toolbar>
-      <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
+    <v-toolbar dark class="purple lighten-2">
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title class="dark">Dev Meetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-xs-only">
         <v-btn flat>
           <v-icon left>supervisor_account</v-icon>
           View Meetups</v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-navigation-drawer v-model="sideNav">
-
+      <v-list>
+        <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>supervisor_account</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>View Meeetup</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
     <main>
 
