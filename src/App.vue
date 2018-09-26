d546c80650d34dced902737eceac68338ba8040e
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-09-26 16:10:20 +0100

    initial project setup

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-title class="dark">Dev Meetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetups</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer v-model="sideNav">
+
+    </v-navigation-drawer>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sideNav: false
+    };
+  }
+};
+</script>
