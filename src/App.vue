78b3fa34ebc1eb7cc149f830e52915d175d46e50
Mubarak Aminu <mubarakaminu340@gmail.com>
2019-03-25 16:23:16 +0100

    create menu object in vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-toolbar dark class="purple lighten-2">
-      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.native.stop="sideNav =  !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title class="dark">Dev Meetup</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
@@ -30,7 +30,14 @@
 export default {
   data() {
     return {
-      sideNav: false
+      sideNav: false,
+      manuItems: [
+        { icon: 'supervisor_account', title: 'View Meetups' },
+        { icon: 'room', title: 'Organize Meetup'},
+        { icon: 'person', title: 'Profile'},
+        { icon: 'face', title: 'Sign up'},
+        { icon: 'lock_opn', title: 'Sign in'}
+      ]
     };
   }
 };
