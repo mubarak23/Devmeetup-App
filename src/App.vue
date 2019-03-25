742b654e6bc8bb9b011bd68d904fdfd32c301955
Mubarak Aminu <mubarakaminu340@gmail.com>
2019-03-25 16:30:00 +0100

    populate the navbar with menuItem object

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,18 @@
       <v-toolbar-title class="dark">Dev Meetup</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>
-          View Meetups</v-btn>
+        <v-btn flat v-for="item in manuItems" :key="item.title">
+          <v-icon left> {{ item.icon }}</v-icon>
+          {{ item.title }}</v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-navigation-drawer v-model="sideNav">
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in manuItems" :key="item.title">
             <v-list-tile-action>
-              <v-icon>supervisor_account</v-icon>
+              <v-icon> {{ item.icon }}</v-icon>
             </v-list-tile-action>
-            <v-list-tile-content>View Meeetup</v-list-tile-content>
+            <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
@@ -36,7 +36,7 @@ export default {
         { icon: 'room', title: 'Organize Meetup'},
         { icon: 'person', title: 'Profile'},
         { icon: 'face', title: 'Sign up'},
-        { icon: 'lock_opn', title: 'Sign in'}
+        { icon: 'lock_open', title: 'Sign in'}
       ]
     };
   }
