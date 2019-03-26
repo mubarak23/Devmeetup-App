3942596807a1f3688fb9148725402face2b415f7
Mubarak Aminu <mubarakaminu340@gmail.com>
2019-03-26 09:16:10 +0100

    create the btn for home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
-<div>
-		The Home Page
-</div>
+<v-container>
+	<v-layout row wrap>
+		 <v-flex x12 sm6 >
+		 	<v-btn large router to="/meetup">Explore Meetup</v-btn>
+		 </v-flex>
+		 <v-flex x12 sm6 >
+		 	<v-btn large router to="/createmeetup">Organize Meetup</v-btn>
+		 </v-flex>
+	</v-layout>
+</v-container>
 
 </template>
