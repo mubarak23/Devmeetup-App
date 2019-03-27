7ca632f60885680d8d053f140b4dd7cfbf302cbf
Mubarak Aminu <mubarakaminu340@gmail.com>
2019-03-27 12:32:17 +0100

    add card element for list of meetup on the view meetups page

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,6 +1,35 @@
 <template>
-<div>
-		The Meetups Page
-</div>
+<v-container>
+	<v-layout row wrap>
+		<v-flex xs12>
+			<v-card class="info">
+				<v-container fluid>
+					<v-layout row>
+					<v-flex xs5 sm4 md3>
+						<v-card-media
+						src="
+						https://media.timeout.com/images/100559575/630/472/image.jpg"
+						height="150px"
+						></v-card-media>
+					</v-flex>
+					<v-flex xs7 sm8 md9>
+						<v-card-title primary-title>
+						  <div>
+						  	<h4 class="white--text md-0">My Meetup</h4>
+							<div> 30th May 2019</div>
+						  </div>
+						</v-card-title>
+						<v-card-actions>
+						<v-btn flat>
+							<v-icon left>arrow_forward</v-icon>
+						 View Meetups </v-btn>	
+						</v-card-actions>
+					</v-flex>
+					</v-layout>
+				</v-container>
+			</v-card>
+		</v-flex>
+	</v-layout>
+</v-container>
 
 </template>
