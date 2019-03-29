cddb260f6ad532fdc0713f20502497fb3b31daf4
Mubarak Aminu <mubarakaminu340@gmail.com>
2019-03-29 19:21:59 +0100

    add vue and inde.js for state management with vuex

---
+++ src/store/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+//use vuex in our uve app
+Vue.use(Vuex)
+
+
+export const store = new Vuex.store({
+
+	state: {
+		loadedMeetups:  [
+			{ imageUrl: 'https://media.timeout.com/images/100559575/630/472/image.jpg',
+			 id:"123ertyh", title: 'meetup in kano', date: '2019-02-27 ' },
+			{ imageUrl: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.travelandleisure.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1600x1000%2Fpublic%2F1475261147%2Ftimes-square-nyc-food-EAT0916.jpg%3Fitok%3DGpQTB9qQ&w=450&c=sc&poi=face&q=85',
+			 , id:"1245ertyh", title: 'meetup in new york', date: '2019-03-20'}
+		],
+		user: {
+			id: '123ert',
+			registeredmeetups: ['123ertyh']
+		}
+
+	},
+
+	muatations: {},
+
+	action: {},
+
+	getter: {
+		loadedMeetups (state){
+			return state.loadedMeetups.sort((meetupA, meetupB) => {
+					return meetupA.date > meetupB.date
+			})
+		},
+		loadedMeetup (state) {
+			return (meetupId) => {
+				return state.loadedMeetups.find(meetup) => {
+					meetup.id === meetupId
+				}
+			}
+		}
+
+	}
+})
